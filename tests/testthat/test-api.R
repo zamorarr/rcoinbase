65782e5202c43e3d07d8b3025354e424d1ec1224
Robert Zamora <zamora.rr@gmail.com>
2017-12-02 12:14:13 -0500

    test values of some request headers

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,21 +1,25 @@
 context("api")
 
 with_mock_API({
-  test_that("api request includes headers", {
+  test_that("api request includes correct headers", {
     resp <- coinbase_api("fake-endpoint")
-    actual <- names(resp$response$request$headers)
-    expected <- c("CB-ACCESS-TIMESTAMP", "CB-ACCESS-KEY", "CB-ACCESS-SIGN",
-                 "CB-VERSION", "Content-Type", "Accept", "User-Agent")
+    actual <- resp$response$request$headers
+    expected_names <- c(
+      "CB-ACCESS-TIMESTAMP", "CB-ACCESS-KEY", "CB-ACCESS-SIGN", "CB-VERSION",
+      "Content-Type", "Accept", "User-Agent")
 
-    expect_true(all(expected %in% actual))
+    expect_true(all(expected_names %in% names(actual)))
+    expect_identical(actual[["Content-Type"]], "application/json")
+    expect_identical(actual[["Accept"]], "application/json")
+    expect_identical(actual[["User-Agent"]], "github.com/zamorarr/rcoinbase")
   })
 })
 
 without_internet({
   test_that("api requests use correct verbs", {
-    expect_GET(coinbase_get("fakeendpoint"))
-    expect_POST(coinbase_post("fakeendpoint"))
-    expect_PUT(coinbase_put("fakeendpoint"))
-    expect_DELETE(coinbase_delete("fakeendpoint"))
+    expect_GET(coinbase_get("fake-endpoint"))
+    expect_POST(coinbase_post("fake-endpoint"))
+    expect_PUT(coinbase_put("fake-endpoint"))
+    expect_DELETE(coinbase_delete("fake-endpoint"))
   })
 })
